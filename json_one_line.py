24f1f320e9a85bb903a5e9947ab4c15eb8a4dec0
Robert Muller <muller.roby@gmail.com>
2017-07-28 11:18:08 +0300

    Add pluggins to format json text

---
+++ json_one_line.py
@@ -0,0 +1,15 @@
+import sublime
+import sublime_plugin
+
+class OneLineCommand(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		view = self.view
+		mysel = self.view.sel()
+		for region in mysel:
+
+			selected_text = view.substr(region)
+			selected_text = selected_text.replace('\n', ' ').replace('\r', '')
+			selected_text = ' '.join(selected_text.split())
+
+			# Replace selected lines with the sorted ones
+			self.view.replace(edit,region,selected_text)
---
+++ json_pretty.py
@@ -0,0 +1,16 @@
+import sublime
+import sublime_plugin
+import json
+
+class JsonFormatCommand(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		view = self.view
+		mysel = self.view.sel()
+		for region in mysel:
+
+			selected_text = view.substr(region)
+			json_object = json.loads(selected_text)
+			selected_text = json.dumps(json_object, indent=4, sort_keys=True)
+
+			# Replace selected lines with the sorted ones
+			self.view.replace(edit,region,selected_text)
