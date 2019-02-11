1b978ce4ed5420cdce64132a7e5e75782b315413
Robert Muller <r.muller@api-international.com>
2019-02-11 20:38:40 +0200

    One line json with no spaces

--- json_one_line.py
+++ json_one_line.py
@@ -8,8 +8,8 @@ class JsonOneLineCommand(sublime_plugin.TextCommand):
 		for region in mysel:
 
 			selected_text = view.substr(region)
-			selected_text = selected_text.replace('\n', ' ').replace('\r', '')
-			selected_text = ' '.join(selected_text.split())
+			selected_text = selected_text.replace('\n', '').replace('\r', '')
+			selected_text = ''.join(selected_text.split())
 
 			# Replace selected lines with the sorted ones
 			view.replace(edit,region,selected_text)
